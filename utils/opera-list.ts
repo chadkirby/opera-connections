873e5423f09a7789dfea77a8d2560a8f5da8cb22
Chad Kirby <chad.kirby@gmail.com>
2022-08-30 21:23:37 -0700

    Omit Barber of Seville by not-Rossini

--- utils/opera-list.ts
+++ utils/opera-list.ts
@@ -29,10 +29,14 @@ export async function getOperaList() {
 
   return new Operas(
     (o) =>
-      !skipComposers.includes(o.composer) &&
-      // Verdi's Otello is much more prominent, and we can't currently
-      // deal with two operas having the same name, so kill the Rossini
-      // version
-      o.titleHref !== '/wiki/Otello_(Rossini)'
+      !(
+        skipComposers.includes(o.composer) ||
+        // Verdi's Otello is much more prominent, and we can't currently
+        // deal with two operas having the same name, so kill the
+        // Rossini version
+        o.titleHref === '/wiki/Otello_(Rossini)' ||
+        // ditto Barber of Seville
+        o.titleHref === '/wiki/Il_barbiere_di_Siviglia_(Paisiello)'
+      )
   );
 }
