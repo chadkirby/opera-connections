dcde33259efcc3e8a83f44b1381f992df18305f1
Chad Kirby <chad.kirby@gmail.com>
2022-10-16 19:57:39 -0700

    select an opera randomly without regard to previous selections
    that's what TT&K asked for

--- netlify/functions/today.ts
+++ netlify/functions/today.ts
@@ -10,54 +10,79 @@ const myHandler: Handler = async (event) => {
   const params = new URLSearchParams(event.rawQuery);
   const todayISO = params.get('today') || DateTime.local().toISO();
 
-  // using luxon, set the origin to midnight July 30, 2022 PDT
-  const origin = DateTime.fromObject(
-    {
-      year: 2022,
-      month: 7,
-      day: 30,
-      hour: 0,
-      minute: 0,
-      second: 0,
-    },
-    {
-      zone: 'America/Los_Angeles',
-    }
-  );
   // set today to midnight of the current day PDT
   const today = DateTime.fromISO(todayISO, { zone: 'America/Los_Angeles' }).set(
     { hour: 0, minute: 0, second: 0 }
   );
-  const daysSinceOrigin = Interval.fromDateTimes(origin, today).count('days');
-  console.error({
-    today: today.toLocaleString(DateTime.DATETIME_MED),
-    origin: origin.toLocaleString(DateTime.DATETIME_MED),
-    daysSinceOrigin,
-  });
-  let targetIndex = (Math.random() * operas.length) | 0;
-  if (!params.get('random')) {
+  // the girls asked for plain-old random selections, so starting
+  // 10/17/2022, just get a random index based on today's date string
+  if (
+    Interval.after(today, 0).isAfter(
+      DateTime.fromObject({ year: 2022, month: 10, day: 16 })
+    )
+  ) {
+    const rng = seedrandom(today.toLocaleString(DateTime.DATETIME_MED));
+    const targetIndex = (rng() * operas.length) | 0;
+    const targetOpera = await operas.getTargetOpera(targetIndex);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        ...targetOpera,
+        today: today.toISO(),
+      } as TargetOpera & {
+        today: string;
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-store, must-revalidate',
+        Vary: '*',
+      },
+    };
+  } else {
+    // use the old-style random number generator
+    // using luxon, set the origin to midnight July 30, 2022 PDT
+    const origin = DateTime.fromObject(
+      {
+        year: 2022,
+        month: 7,
+        day: 30,
+        hour: 0,
+        minute: 0,
+        second: 0,
+      },
+      {
+        zone: 'America/Los_Angeles',
+      }
+    );
+    const daysSinceOrigin = Interval.fromDateTimes(origin, today).count('days');
+    console.error({
+      today: today.toLocaleString(DateTime.DATETIME_MED),
+      origin: origin.toLocaleString(DateTime.DATETIME_MED),
+      daysSinceOrigin,
+    });
+    let targetIndex = 0;
     const rng = seedrandom('shuffle-operas-1');
     for (let d = 0; d < daysSinceOrigin; d++) {
       targetIndex = (rng() * operas.length) | 0;
     }
+
+    const targetOpera = await operas.getTargetOpera(targetIndex);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        ...targetOpera,
+        today: today.toISO(),
+      } as TargetOpera & {
+        today: string;
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-store, must-revalidate',
+        Vary: '*',
+      },
+    };
   }
-
-  const targetOpera = await operas.getTargetOpera(targetIndex);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      ...targetOpera,
-      today: today.toISO(),
-    } as TargetOpera & {
-      today: string;
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      'Cache-Control': 'no-store, must-revalidate',
-      Vary: '*',
-    },
-  };
 };
 
 const handler = builder(myHandler);
